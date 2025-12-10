45c763fc0b6bccb5456232b80257f8d070faddbf
ShirishBhor <shirishb258@gmail.com>
2025-12-10 12:57:43 +0530

    Changes

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -3,6 +3,7 @@ import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Employee Management System"
+    # DATABASE_URL: str = "mysql+pymysql://root:root@localhost/empl"
     DATABASE_URL: str = "mysql+pymysql://root:root@localhost/emple"
     JWT_SECRET: str = "supersecretjwtkey"
     JWT_ALGORITHM: str = "HS256"
