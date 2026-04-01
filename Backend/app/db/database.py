63124aaaad4bf3ade89cc45dd0074b48119802d2
ShirishBhor <shirishb258@gmail.com>
2026-04-01 15:25:21 +0530

    DB Pool new

--- Backend/app/db/database.py
+++ Backend/app/db/database.py
@@ -6,10 +6,12 @@ engine = create_engine(
     settings.DATABASE_URL,
     echo=False,
     pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=10,
+    pool_size=5,
+    max_overflow=5,
     pool_timeout=30,
     pool_recycle=1800,
+    pool_use_lifo=True,
+    pool_reset_on_return="rollback"
 )
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
